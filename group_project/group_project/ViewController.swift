909aef26e55cb31386ff80180c5e1dff56499f01
Haley <hs.haleyk@gmail.com>
2025-02-14 11:38:58 -0500

    Initial commit with story board design

---
+++ group_project/group_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  group_project
+//
+//  Created by Haley Kim on 2025-02-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
