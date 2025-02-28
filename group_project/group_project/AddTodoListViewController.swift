59a062202efd633a92fb822db874c514e5eba237
Haley <hs.haleyk@gmail.com>
2025-02-27 23:19:45 -0500

    add todo list screens

---
+++ group_project/group_project/AddTodoListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AddTodoListViewController.swift
+//  group_project
+//
+//  Created by Aadi Badola on 2025-02-26.
+//
+
+import UIKit
+
+protocol AddItemDelegate: AnyObject {
+    func didAddNewItem(_ item: String)
+}
+
+class AddTodoListViewController: UIViewController {
+
+    @IBOutlet weak var todoListTextField: UITextField!
+    
+    weak var delegate: AddItemDelegate?
+    
+    @IBAction func confirmButtonTapped(_ sender: UIButton) {
+        if let newItem = todoListTextField.text, !newItem.isEmpty {
+            delegate?.didAddNewItem(newItem)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ group_project/group_project/TodoListTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  TodoListTableViewController.swift
+//  group_project
+//
+//  Created by Aadi Badola on 2025-02-26.
+//
+
+import UIKit
+
+class TodoListTableViewController: UITableViewController, AddItemDelegate {
+    @IBOutlet var addTodoList: UITableView!
+    
+    var todoLists: [String] = ["Buy Groceries", "Complete COMP3097 Project", "Go To Gym"];
+    var sections = ["Section 1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoListCell");
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return self.sections.count;
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return todoLists.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = todoLists[indexPath.row]
+        return cell
+    }
+
+    func didAddNewItem(_ item: String) {
+        todoLists.append(item)
+        tableView.reloadData()
+    }
+    
+    // MARK: - Navigation
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showTodoItemsSegue", sender: nil)
+    }
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "showTodoItemsSegue",
+           let destinationVC = segue.destination as? TodoItemsTableViewController,
+           let selectedRow = tableView.indexPathForSelectedRow?.row {
+                destinationVC.todoTitle = todoLists[selectedRow]
+        }
+        if segue.identifier == "addTodoListSegue",
+           let destinationVC = segue.destination as? AddTodoListViewController {
+            destinationVC.delegate = self
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+
+}
