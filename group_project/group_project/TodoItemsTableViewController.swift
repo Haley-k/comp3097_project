c542306cf6297545974ed405b7bd0ee6b0cc91f2
Aadi <aaadibadola@gmail.com>
2025-03-16 14:54:49 -0400

    update TodoItemsTable

--- group_project/group_project/TodoItemsTableViewController.swift
+++ group_project/group_project/TodoItemsTableViewController.swift
@@ -7,123 +7,134 @@
 
 import UIKit
 
-class TodoItemsTableViewController: UITableViewController, AddTodoDelegate {
 
+protocol AddTodoDelegate: AnyObject {
+    func didAddTodo(item: TodoItem)
+}
+
+class TodoItemsTableViewController: UITableViewController, UISearchBarDelegate, AddTodoDelegate {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var titleTextView: UITextView!
     
-    var todoTitle: String?
-    var todoItems: [(title: String, dueDate: String)] = [
-        ("Todo 1", "Due: March 1, 2025"),
-        ("Todo 2", "Due: March 5, 2025"),
-        ("Todo 3", "Due: March 10, 2025")
-    ]
+    var filteredItems: [TodoItem] = []
+    var isSearchActive: Bool = false
+    var categoryName: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        titleTextView?.textContainer.maximumNumberOfLines = 1
-        titleTextView?.textContainer.lineBreakMode = .byTruncatingTail
-        if let title = todoTitle {
-            titleTextView?.text = title;
-            self.navigationItem.title = title;
-            self.title = title;
+        if let catName = categoryName {
+            titleTextView?.text = catName;
+            navigationItem.title = catName
         }
-        
     }
     
-    func didAddTodo(title: String, dueDate: String) {
-        todoItems.append((title, dueDate))
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let catName = categoryName else { return }
+        
+        if searchText.isEmpty {
+            isSearchActive = false
+        } else {
+            isSearchActive = true
+            filteredItems = TodoDataManager.shared.getTodoItems(for: catName).filter {
+                $0.title.lowercased().contains(searchText.lowercased())
+            }
+        }
         tableView.reloadData()
     }
-
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearchActive = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    func didAddTodo(item: TodoItem) {
+        TodoDataManager.shared.addTodoItem(item)
+        dismiss(animated: true) {
+            self.tableView.reloadData()
+        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return todoItems.count
+        guard let catName = categoryName else { return 0 }
+        return isSearchActive ? filteredItems.count : TodoDataManager.shared.getTodoItems(for: catName).count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
-        // Configure the cell...
-        let todoItem = todoItems[indexPath.row]
+        
+        guard let catName = categoryName else { return cell }
+        
+        let todoItem = isSearchActive ? filteredItems[indexPath.row] : TodoDataManager.shared.getTodoItems(for: catName)[indexPath.row]
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        let dueDateString = formatter.string(from: todoItem.dueDate)
+        
+        let today = Date()
+        let calendar = Calendar.current
+        let daysRemaining = calendar.dateComponents([.day], from: today, to: todoItem.dueDate).day ?? 0
+        
+        let dueDateColor: UIColor = {
+            if daysRemaining < 0 {
+                return .red // past due
+            } else if daysRemaining < 3 {
+                return .blue // due in 3 days
+            }
+            else {
+                return .gray // defult
+            }
+        }()
+        
+        let attributedText = NSMutableAttributedString(string: "Due: ", attributes: [.foregroundColor: UIColor.black])
+        let dueDateAttributedString = NSAttributedString(string: dueDateString, attributes: [.foregroundColor: dueDateColor])
+        attributedText.append(dueDateAttributedString)
+        
         cell.textLabel?.text = todoItem.title
-        cell.detailTextLabel?.text = todoItem.dueDate
-
+        cell.detailTextLabel?.attributedText = attributedText
+        
         return cell
     }
     
-    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
         if segue.identifier == "showTodoDetailSegue" {
-            if let destinationVC = segue.destination as? TodoDetailViewController,
-               let selectedRow = tableView.indexPathForSelectedRow?.row {
-                   destinationVC.todoTitle = todoItems[selectedRow].title
-                   destinationVC.todoDue = todoItems[selectedRow].dueDate
-               }
+            if let detailVC = segue.destination as? TodoDetailViewController,
+               
+                let index = tableView.indexPathForSelectedRow?.row,
+               let catName = categoryName {
+                let items = TodoDataManager.shared.getTodoItems(for: catName)
+                detailVC.todoItem = items[index]
+            }
         }
-        if segue.identifier == "newTodoSegue",
-           let addTodoVC = segue.destination as? AddTodoViewController {
-                addTodoVC.delegate = self
+        else if segue.identifier == "newTodoSegue",
+                let addTodoVC = segue.destination as? AddTodoViewController {
+            addTodoVC.delegate = self
+            addTodoVC.allCategories = TodoDataManager.shared.categories
+            addTodoVC.defaultCategory = categoryName
         }
     }
     
     @IBAction func unwindToTodoItemsTableViewController(_ segue: UIStoryboardSegue) {
-        if let sourceVC = segue.source as? AddTodoViewController, let newTitle = sourceVC.todoTitle, let newTodoDue = sourceVC.todoDueDate {
-            todoItems.append((title: newTitle, dueDate: newTodoDue))
-            tableView.reloadData()
-        }
+        
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-
 }
+
